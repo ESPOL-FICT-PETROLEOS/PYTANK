8f56cac3c29355a0731ad6f05af27b1a4a8c54ab
ErickVillarroel <evillarr@espol.edu.ec>
2024-05-02 18:16:29 -0500

    Modifications: get_wells: test of ModelFluid constants: add columns of pvt properties to Press Schema...

--- new/get_wells.py
+++ new/get_wells.py
@@ -7,23 +7,30 @@ from new.constants import (OIL_CUM_COL,
                            LIQ_CUM,
                            PRESSURE_COL,
                            TANK_COL,
-                           DATE_COL)
+                           DATE_COL,
+                           OIL_FVF_COL,
+                           GAS_FVF_COL,
+                           RS_COL)
 from new.vector_data import ProdVector, PressVector
 from new.well import Well
 from new.utilities import normalize_date_freq
 from collections import defaultdict
 from pandera.errors import SchemaError
 from uw import underground_widrawal
+from new.fluid import FluidModel
 
-# Data to process with production info
+# Data to process production info
 df_production = pd.read_csv("../old/tests/data_for_tests/full_example_1/production.csv")
 df_production[DATE_COL] = pd.to_datetime(df_production[DATE_COL])
 df_production.set_index(df_production[DATE_COL], inplace=True)
 
-# Data to process with pressure info
+# Data to process pressure info
 df_pressures = pd.read_csv("../old/tests/data_for_tests/full_example_1/pressures.csv")
 df_pressures.rename(columns={"DATE": "START_DATETIME", "WELLLBORE": "ITEM_NAME"}, inplace=True)
 df_pressures["START_DATETIME"] = pd.to_datetime(df_pressures["START_DATETIME"])
+
+# Data to process pvt info
+df_pvt = pd.read_csv("../old/tests/data_for_tests/full_example_1/pvt.csv")
 
 # Empty dictionary for the different tanks
 tank_wells = defaultdict(list)
@@ -70,10 +77,26 @@ for name, group_prod in df_production.groupby("ITEM_NAME"):
     if name in df_pressures["WELLBORE"].unique():
         group_press = df_pressures[df_pressures["WELLBORE"] == name]
 
+        # Interpolated pvt properties
+        pvt = FluidModel(
+            data_pvt=df_pvt
+        )
+
+        # Create a copy of group_press to avoid warnings
+        group_press = group_press.copy()
+
+        # Add columns of pvt properties
+        group_press[OIL_FVF_COL] = pvt.get_bo_at_press(group_press[PRESSURE_COL])
+        group_press[GAS_FVF_COL] = pvt.get_bg_at_press(group_press[PRESSURE_COL])
+        group_press[RS_COL] = pvt.get_rs_at_press(group_press[PRESSURE_COL])
+
         # Renaming columns of pressure data
         group_press = group_press.rename(
             columns={
-                PRESSURE_COL: PRESSURE_COL
+                PRESSURE_COL: PRESSURE_COL,
+                OIL_FVF_COL: OIL_FVF_COL,
+                GAS_FVF_COL: GAS_FVF_COL,
+                RS_COL: RS_COL,
             }
         )
         group_press.set_index("START_DATETIME", inplace=True)
