793a8eb97584482ea277d92c4c7ea7ca54984516
ErickVillarroel <evillarr@espol.edu.ec>
2024-05-31 16:49:38 -0500

    Creation of internal methods for the calculation of uw and advance of the volumetric average function

--- new/fluid.py
+++ new/fluid.py
@@ -1,41 +1,30 @@
 import numpy as np
 import pandas as pd
 from typing import Optional, Callable
-from pydantic import BaseModel, field_validator
+from pydantic import BaseModel
 from scipy.interpolate import interp1d
-from constants import (PRESSURE_PVT_COL,
-                       OIL_FVF_COL,
-                       GAS_FVF_COL,
-                       RS_COL,
-                       )
+import pandera as pa
+from pandera.typing import DataFrame, Series
+from new.constants import (PRESSURE_PVT_COL,
+                           OIL_FVF_COL,
+                           GAS_FVF_COL,
+                           RS_COL
+                           )
 
 
-class FluidModel(BaseModel):
-    data_pvt: pd.DataFrame
+class PVTSchema(pa.DataFrameModel):
+    Pressure: Series[float] = pa.Field(ge=0, unique=True, coerce=True, nullable=False)
+    Bo: Series[float] = pa.Field(ge=0, coerce=True)
+    Bg: Series[float] = pa.Field(ge=0, coerce=True, nullable=True)
+    GOR: Series[float] = pa.Field(ge=0, coerce=True)
+
+
+class OilModel(BaseModel):
+    data_pvt: DataFrame[PVTSchema]
     temperature: float
 
     class Config:
         arbitrary_types_allowed = True
-
-    @classmethod
-    def validate_data_pvt(cls, value):
-        """Validate that the corresponding columns exist"""
-        required_columns = {PRESSURE_PVT_COL, OIL_FVF_COL, GAS_FVF_COL, RS_COL}
-        if not required_columns.issubset(value.columns):
-            raise ValueError(f"DataFrame must have columns: {PRESSURE_PVT_COL}, "
-                             f"{OIL_FVF_COL}, "
-                             f" {GAS_FVF_COL}, "
-                             f"{RS_COL}")
-
-        """Validate the values found in each column of the DataFrame"""
-        for column in value.columns:
-            if not all(isinstance(val, (float, int)) and val >= 0 or np.isnan(val) for val in value[column]):
-                raise ValueError(f"Invalid values in columns '{column}'")
-        return value
-
-    @field_validator("data_pvt")
-    def check_data_pvt(cls, value):
-        return cls.validate_data_pvt(value)
 
     def _interpolated_column_at_pressure(self, column_name: str, pressure: float) -> float:
         df_pvt_local = self.data_pvt
@@ -61,7 +50,7 @@ df_pvt_inv = pd.DataFrame({
     RS_COL: [10.3, 34, 50],
 })
 
-validation = FluidModel(
+validation = OilModel(
     data_pvt=df_pvt_inv,
     temperature=25
 )
@@ -69,52 +58,36 @@ validation = FluidModel(
 # %%
 # Quicktest for some method
 df_pvt = pd.read_csv("../old/tests/data_for_tests/full_example_1/pvt.csv")
-fluid = FluidModel(
+fluid = OilModel(
     data_pvt=df_pvt,
     temperature=25
 )
 
-#print(f"Bo is: {fluid.get_bo_at_press(np.array([100,200,300]))}")
-#print(f"Bg is: {fluid.get_bg_at_press(100)}")
-#print(f"GOR is: {fluid.get_rs_at_press(100)}")
+print(f"Bo is: {fluid.get_bo_at_press(np.array([100,200,300]))}")
+print(f"Bg is: {fluid.get_bg_at_press(100)}")
+print(f"GOR is: {fluid.get_rs_at_press(100)}")
 
-
-# %%
-# Working...
-class OilFluidModel(FluidModel):
-    specific_gravity: Optional[float] = None
-
-    def interp_table(self) -> pd.DataFrame:
-        pass
-
-    def get_bo_at_press(self, pressure) -> float:
-        pass
-
-    def get_bg_at_press(self, pressure) -> float:
-        pass
-
-    def get_rs_at_press(self, pressure) -> float:
-        pass
-
-
-class WaterFluidModel(BaseModel):
-    #salinity: float = 3000
-    correlation: Callable
-    #temperature: float = 220
-    #unit: Optional[int] = 1
+#%%
+class WaterModel(BaseModel):
+    correlation_bw: Callable
+    correlation_rs: Callable
+    salinity: float
+    temperature: float
+    unit: float
 
     def interp_table(self) -> pd.DataFrame:
         pass
 
-    def get_bw_at_press(self, pressure: float, temperature: float, salinity: float, unit: int = 1) -> float:
-        bw = self.correlation(pressure, temperature, salinity, unit)
+    def get_bw_at_press(self, pressure: float) -> float:
+        bw = self.correlation_bw(pressure, self.temperature, self.salinity, self.unit)
         return bw
 
-    def get_rs_at_press(self, pressure: float,temperature: float, salinity: float, unit: int = 1) -> float:
-        rs = self.correlation(pressure, temperature, salinity, unit)
+    def get_rs_at_press(self, pressure: float) -> float:
+        rs = self.correlation_rs(pressure, self.temperature, self.salinity, self.unit)
         return rs
 
-class FullFluidModel(BaseModel):
-    oil: OilFluidModel
-    water: WaterFluidModel
+
+class FluidModel(BaseModel):
+    oil: OilModel
+    water: WaterModel
 
