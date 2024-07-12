4a79cd0bee045dbc841194c4529220aecd886d82
ErickVillarroel <evillarr@espol.edu.ec>
2024-07-12 14:00:10 -0500

    Improvement plots

--- examples_data/read_data.py
+++ examples_data/read_data.py
@@ -1,3 +1,8 @@
+"""
+read_data.py
+
+This script is to convert to DataFrame the csv data that user can use.
+"""
 import pandas as pd
 production_data = pd.read_csv("../examples_data/production.csv")
 pressure_data = pd.read_csv("../examples_data/pressures.csv")
