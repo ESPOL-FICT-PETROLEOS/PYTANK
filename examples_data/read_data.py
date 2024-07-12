343a54cdc07ec3d1d9fe45d1c42a1710a3ca58a2
ksls2000 <kelosori@espol.edu.ec>
2024-07-11 23:08:59 -0500

    reformat data example

--- examples_data/read_data.py
+++ examples_data/read_data.py
@@ -2,3 +2,4 @@ import pandas as pd
 production_data = pd.read_csv("../examples_data/production.csv")
 pressure_data = pd.read_csv("../examples_data/pressures.csv")
 pvt_data = pd.read_csv("../examples_data/pvt.csv")
+injection_data = pd.read_csv("../examples_data/injection.csv")
